cd9efec812ec2d1f199b47b102acbf7b3c532f1e
mjmcode <mjm7542>
2023-07-01 10:06:41 +0900

    test3

--- middlewares/auth-middlewares.js
+++ middlewares/auth-middlewares.js
@@ -3,22 +3,22 @@ const User = require("../schemas/user");
 
 // 사용자 인증 미들웨어
 module.exports = async (req, res, next) => {
+
+    const { authorization } = req.cookies;
+    const [loginType, loginToken] = (authorization ?? "").split(" ");
+
+    if (!loginToken || loginType !== "Bearer") {
+        res.status(402).json({
+            errorMessage: "로그인이 필요한 기능입니다.",
+        });
+        return;
+    }
+
     try {
-        const { authorization } = req.cookies;
-        const [loginType, loginToken] = (authorization ?? "").split(" ");
-
-        if (!loginToken || loginType !== "Bearer") {
-            res.status(402).json({
-                errorMessage: "로그인이 필요한 기능입니다.",
-            });
-            return;
-        }
-
         const { userId } = jwt.verify(loginToken, "customized-secret-key");
         const user = await User.findById(userId);
         res.locals.user = user;
         next();
-
     } catch (error) {
         res.status(403).send({
             errorMessage: "전달된 쿠키에서 오류가 발생하였습니다.",
