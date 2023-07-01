2922c9051df0ac621bec5cc70fe4f7d3e92b9c86
mjmcode <mjm7542>
2023-07-01 10:01:08 +0900

    test2

--- routes/login.js
+++ routes/login.js
@@ -6,24 +6,25 @@ const User = require("../schemas/user");
 
 // 로그인 API
 router.post("/login", async (req, res) => {
+
+    const { nickname, password } = req.body;
+    const user = await User.findOne({ nickname });
+
+    if (!user || password !== user.password) {
+        res.status(412).json({
+            errorMessage: "닉네임 또는 패스워드를 확인해주세요."
+        });
+        return;
+    }
+
+    // JWT 생성
+    const token = jwt.sign(
+        { userId: user.userId },
+        "customized-secret-key",
+        // 시크릿키는 내일 조정
+    );
+
     try {
-        const { nickname, password } = req.body;
-        const user = await User.findOne({ nickname });
-
-        if (!user || password !== user.password) {
-            res.status(412).json({
-                errorMessage: "닉네임 또는 패스워드를 확인해주세요."
-            });
-            return;
-        }
-
-        // JWT 생성
-        const token = jwt.sign(
-            { userId: user.userId },
-            "customized-secret-key",
-            // 시크릿키는 내일 조정
-        );
-
         res.cookie("authorization", "Bearer " + token); //
         res.status(200).json({ token });
     } catch (error) {
